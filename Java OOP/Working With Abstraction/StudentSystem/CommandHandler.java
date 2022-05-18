b6863ec19b5bee3e419e168a818039b581ba835e
Diana <98754657+dianabanderska@users.noreply.github.com>
2022-05-18 11:18:05 +0300

    Add files via upload

---
+++ Java OOP/Working With Abstraction/StudentSystem/CommandHandler.java
@@ -0,0 +1,26 @@
+package StudentSystem;
+
+public class CommandHandler {
+    private StudentRepository repository;
+
+    public CommandHandler() {
+        this.repository = new StudentRepository();
+    }
+
+    public String handleCommand(String[] tokens) {
+        if (tokens[0].equals("Create")) {
+            String name = tokens[1];
+            int age = Integer.parseInt(tokens[2]);
+            double grade = Double.parseDouble(tokens[3]);
+            Student student = new Student(name, age, grade);
+            this.repository.add(student);
+            return null;
+        } else if (tokens[0].equals("Show")) {
+            String name = tokens[1];
+            if (repository.contains(name)) {
+                return repository.get(name).getInfo();
+            }
+        }
+            return tokens[0];
+    }
+}
---
+++ Java OOP/Working With Abstraction/StudentSystem/ConsolePrinter.java
@@ -0,0 +1,8 @@
+package StudentSystem;
+
+public class ConsolePrinter {
+
+    public static void printLine(String str) {
+        System.out.println(str);
+    }
+}
