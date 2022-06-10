a32b0a295b5f511c08c4dc53b7092437651b29f2
Diana <98754657+dianabanderska@users.noreply.github.com>
2022-06-10 13:29:24 +0300

    Add files via upload

---
+++ Algorithms Fundamentals/Recursion/ReversedRecursion.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReversedRecursion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = sumNumbers(arr, arr.length - 1);
+        System.out.println(sum);
+    }
+
+    private static int sumNumbers(int[] arr, int i) {
+        if (i < 0) {
+            return 0;
+        }
+        return arr[i] + sumNumbers(arr, i - 1);
+    }
+}
