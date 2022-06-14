2633eebe0443db0e4311f1b15729c4d59d8cd4f5
Diana <98754657+dianabanderska@users.noreply.github.com>
2022-06-14 13:05:14 +0300

    Add files via upload

---
+++ JavaScript Advanced/SumOfNumbersNtoM.js
@@ -0,0 +1,14 @@
+function solve(a, b) {
+    let sum = 0;
+    const N = Number(a);
+    const M = Number(b);
+
+    for (let i = N; i <= M; i++) {
+        sum += i;
+    }
+    console.log(sum)
+}
+
+solve(
+    '1', '5'
+)
