4ca507ecb6db81899a6cad8156876db9039d9ade
Diana <98754657+dianabanderska@users.noreply.github.com>
2022-06-29 23:29:53 +0300

    Add files via upload

--- Algorithms Fundamentals/Recursion/FindAllPathsInLabyrinth.java
+++ Algorithms Fundamentals/Recursion/FindAllPathsInLabyrinth.java
@@ -18,31 +18,31 @@ public class FindAllPathsInLabyrinth {
         for (int i = 0; i < rows; i++) {
             labyrinth[i] = scanner.nextLine().toCharArray();
         }
-        findPath(labyrinth, 0, 0, ' ');
+        findPath(labyrinth, 0, 0, ' ');// ' ' tuk dobavqme edin char parametur,koito shte ni dava kakva posoka sme izbrali,zashtoto po uslovie trqbva da otpechatvame naude sme trugnali napr R za nadqsno L za nalqvo i t.n.
     }
 
     private static void findPath(char[][] labyrinth, int row, int col, char direction) {
         if (!isInBounds(labyrinth, row, col) || labyrinth[row][col] == 'V'
                 || labyrinth[row][col] == '*') {
-            return;
+            return;//tuk kazvame,che,ako izleze ot ramkite na matricata shte trqbva da returne, t.e. spira i nqma kakvo da tursi natatuk!
         }
         path.add(direction);
 
         if (labyrinth[row][col] == 'e') {
             printPath();
             path.remove(path.size() - 1);
-            return;
+            return;//tuk imame return inache shte markira i izhoda i nikoga nqma da imame 'e' sled '-', zashtoto shte go setnem na prazna kletka
         }
-        labyrinth[row][col] = 'V';
+        labyrinth[row][col] = 'V';//tuk markirame kato poseteno vseki hod
 
-        findPath(labyrinth, row, col - 1, 'L');
-        findPath(labyrinth, row, col + 1, 'R');
-        findPath(labyrinth, row - 1, col, 'U');
-        findPath(labyrinth, row + 1, col, 'D');
+        findPath(labyrinth, row, col - 1, 'L');// s col - 1 hodim nalqvo ot kolonite
+        findPath(labyrinth, row, col + 1, 'R');// s col + 1 hodim nadqsno ot kolonite
+        findPath(labyrinth, row - 1, col, 'U');//(s row - 1 hodim nagore)tuk izvikvame rekursiqta,za vsqka sledvashta stupka se pravi s rekursiq(izvikvame q 4 puti,zashtoto iskame da probvame da hodim i v 4te posoki)rekursiqta shte dulbae,dokato ne sreshtne stena(nie si q opredelqme s proverki)sreshtne li shte spre da dulbae i shte ni returne(shte se vurne i zapochva na novo da probva v sledvashtata posoka)
+        findPath(labyrinth, row + 1, col, 'D');//s row + 1 hodim nadolu
 
-        labyrinth[row][col] = '-';
+        labyrinth[row][col] = '-';//tuk izvikvame backtrackinga t.e. sled kato se vrushtame ot rekursiite markirame vsqka kletka,koqto sme uspqli da posetim purvo kato posetena s 'V' i sled tova kato posetena s '-'(t.e. otmarkirame vse edno poseshtenieto i q markirame kato svobodna '-')(tezi znaci sa ni po uslovie)
 
-        path.remove(path.size() - 1);
+        path.remove(path.size() - 1);//vseki put kogato backtrackvame bez da sme namerili izhoda, premahvame poslednata posoka(posledniq index),po koqto sme bili trugnali
     }
 
     private static void printPath() {
